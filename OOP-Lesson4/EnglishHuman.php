39a3494741d4a47308086a049fde909b42fa0eb3
retmix <jameslayng123@gmail.com>
2022-11-08 22:29:48 +0700

    update

---
+++ OOP-Lesson4/EnglishHuman.php
@@ -0,0 +1,15 @@
+<?php
+require_once "HumanAbstract.php";
+class EnglishHuman extends HumanAbstract
+{
+
+    public function getGreetings(): string
+    {
+        return "Hello";
+    }
+
+    public function getMyNameIs(): string
+    {
+        return "My name is ".$this->getName();
+    }
+}
